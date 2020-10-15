613b0eaba43b70daabdbf5350639bc9654f991bc
ShaderOX <obsidianic18k@gmail.com>
2020-10-15 16:36:45 +0500

    v2.5 Added Automated Process

--- install.sh
+++ install.sh
@@ -12,5 +12,6 @@ cp cget.py utils.py ../
 python3 install -r requirements.txt
 cd ..
 rm -rf ./CGet/
-echo "alias cget=python3 \"$HOME/$NEW_DIR_NAME/cget.py\"" >> "$HOME/.bashrc"
+echo "\"$HOME/$NEW_DIR_NAME/cget.py\""
+echo "alias cget=\"python3 \"$HOME/$NEW_DIR_NAME/cget.py\"\"" >> "$HOME/.bashrc"
 echo Done!
