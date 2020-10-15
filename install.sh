a5529fa000878e1a4f195e47dea26fbc9df90d8f
ShaderOX <obsidianic18k@gmail.com>
2020-10-15 17:02:41 +0500

    v2.5 Added Automated Process

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ cd $HOME
 mkdir "$NEW_DIR_NAME"
 cd "$NEW_DIR_NAME"
 
+NEW_DIR_PATH="$HOME/$NEW_DIR_NAME"
 git clone https://github.com/ShaderOX/CGet.git
 cd CGet
 cp cget.py utils.py ../
 python3 install -r requirements.txt
 cd ..
 rm -rf ./CGet/
-echo "\"$HOME/$NEW_DIR_NAME/cget.py\""
-echo "alias cget=\"python3 \"$HOME/$NEW_DIR_NAME/cget.py\"\"" >> "$HOME/.bashrc" 
+echo "alias cget=\"python3 '$NEW_DIR_PATH/cget.py'\"" >> "$HOME/.bashrc" 
 echo Done!
